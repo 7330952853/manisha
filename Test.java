67cfc864e5b1ce0fd22c47aa45edcddb01743c33
7330952853 <128679595+7330952853@users.noreply.github.com>
2023-12-07 22:29:58 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test{
+  public static void main(String arg[]){
+    System.out.println("enter your name:");
+    Scanner sc=new Scanner(System.in);
+    int s=sc.nextLine();
+    System.out.println("My Name is :"+s);
+  }
+  ]
+    
